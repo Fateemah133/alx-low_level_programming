427e434c9bfd013b3d879eafbb5a517a116c9fa3
Fateemah <fateemahoyeniran@gmail.com>
2023-08-14 09:17:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Progrma to print alphabet in lowercase followed by a new line using putchar function
+ *
+ * Return: Always (0)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alphabet = 'a';
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
