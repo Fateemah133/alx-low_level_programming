e740284bb843e46790f9a8ccc4042e03a9dcffc8
Fateemah <fateemahoyeniran@gmail.com>
2023-08-15 04:26:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that print 10 times in alphabet
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+		for (alphabet = 0; alphabet < 10; alphabet++)
+		{
+	    putchar(alphabet)
+		}
+	putchar('\n');
+	return (0);
+}
