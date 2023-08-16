75b442c64c1ec18f4c7c34294b170ec7ad708ac1
Fateemah <fateemahoyeniran@gmail.com>
2023-08-15 21:32:21 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void)
+int _islower(int c);
+int _isalpha(int c);
+#endif /*MAIN_HEADER_FILE*/
+
