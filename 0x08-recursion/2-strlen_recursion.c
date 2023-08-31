c6171a12216ca0bb0fb85784571285f5a781e7bb
Fateemah <fateemahoyeniran@gmail.com>
2023-08-31 07:30:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - functions that return the length of a string
+ * @s: input
+ * return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
